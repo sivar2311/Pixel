5bd1d8a5a75e0eab3a234080c69084831fe99ccc
sivar2311 <sivar2311@gmail.com>
2022-11-22 15:59:22 +0100

    clean code implementation

--- src/Pixel.cpp
+++ src/Pixel.cpp
@@ -2,8 +2,10 @@
 
 #include "PixelManager.h"
 
+// public functions
+
 Pixel::Pixel(CRGB& led)
-    : _led(led) {
+    : led(led) {
     PixelManager::add(this);
 }
 
@@ -12,80 +14,141 @@ Pixel::~Pixel() {
 }
 
 Pixel& Pixel::blink(const CRGB& color, unsigned long interval, unsigned int count) {
-    _pixel_mode = PixelMode_t::blink;
-
-    _color            = color;
-    _background_color = CRGB::Black;
-
-    _blink_info = {
-        .interval      = interval,
-        .off_interval  = interval,
-        .blink_count   = count,
-        .blink_counter = 1,
-        .last_blink    = 0,
-        .state         = false,
-    };
-
+    this->pixel_color        = color;
+    this->pixel_bg_color     = CRGB::Black;
+    this->blink_on_interval  = interval;
+    this->blink_off_interval = interval;
+    this->blink_counter      = 0;
+    this->blink_count        = count;
+    this->pixel_mode         = PM_BLINK;
+    this->pixel_state        = false;
+    this->last_millis        = 0;
     return *this;
 }
 
-Pixel& Pixel::color(const CRGB& color) {
-    _pixel_mode = steady;
-    _color      = color;
-    _led        = color;
-
-    PixelManager::update();
+Pixel& Pixel::color(const CRGB color) {
+    this->pixel_state = false;
+    this->pixel_color = color;
+    this->pixel_mode  = PM_PERMANENT;
     return *this;
 }
 
-Pixel& Pixel::backgroundColor(const CRGB& color) {
-    _background_color = color;
+Pixel& Pixel::shift(unsigned long ms) {
+    this->shift_time = millis() + ms;
     return *this;
+}
+
+Pixel& Pixel::bgColor(const CRGB bgColor) {
+    this->pixel_bg_color = bgColor;
+    return *this;
+}
+
+Pixel& Pixel::off_interval(unsigned long interval) {
+    this->blink_off_interval = interval;
+    return *this;
+}
+
+// clean code helper functions
+
+inline bool Pixel::pixel_is_shifted() {
+    if ((shift_time > 0) && (millis() < shift_time)) return true;
+    shift_time = 0;
+    return false;
+}
+
+inline bool Pixel::pixel_is_in_blink_mode() {
+    return pixel_mode == PM_BLINK;
+}
+
+inline bool Pixel::pixel_is_not_in_blink_mode() {
+    return pixel_mode != PM_BLINK;
+}
+
+inline bool Pixel::pixel_is_in_permanent_mode() {
+    return pixel_mode == PM_PERMANENT;
+}
+
+inline bool Pixel::pixel_is_not_in_permanent_mode() {
+    return pixel_mode != PM_PERMANENT;
+}
+
+inline unsigned long Pixel::get_wait_time() {
+    return pixel_state ? blink_on_interval : blink_off_interval;
+}
+
+inline bool Pixel::blink_time_reached() {
+    if (last_millis == 0) {
+        last_millis = millis();
+        return true;
+    }
+
+    if (millis() - last_millis >= get_wait_time()) {
+        last_millis = millis();
+        return true;
+    }
+    return false;
+}
+
+inline bool Pixel::blink_time_not_reached() {
+    return !blink_time_reached();
+}
+
+inline void Pixel::set_pixel_color_to_current_blink_state() {
+    CRGB& color = pixel_state ? pixel_color : pixel_bg_color;
+    set_pixel_color(color);
+}
+
+inline void Pixel::flip_state() {
+    pixel_state = !pixel_state;
+}
+
+inline void Pixel::increment_blink_counter() {
+    if (blink_count > 0 && pixel_state == false) blink_counter++;
+}
+
+inline bool Pixel::blink_counter_is_at_maximum() {
+    if (!blink_count) return false;
+    return blink_count == blink_counter;
+}
+
+inline void Pixel::stop_blinking() {
+    pixel_mode = PM_OFF;
+}
+
+inline bool Pixel::state_is_off() {
+    return pixel_state == false;
+}
+
+inline bool Pixel::state_is_on() {
+    return pixel_state == true;
+}
+
+inline void Pixel::set_pixel_color(const CRGB& color) {
+    led = color;
+    PixelManager::force_update();
+}
+
+// work functions
+
+void Pixel::handle_blink_mode() {
+    if (blink_time_not_reached()) return;
+
+    flip_state();
+    set_pixel_color_to_current_blink_state();
+
+    increment_blink_counter();
+    if (blink_counter_is_at_maximum()) stop_blinking();
+}
+
+void Pixel::handle_permanent_mode() {
+    if (state_is_off()) {
+        set_pixel_color(pixel_color);
+        pixel_state = true;
+    }
 }
 
 void Pixel::_loop() {
-    unsigned long current_millis = millis();
-
-    if (_pixel_mode == PixelMode_t::shifted && current_millis >= _shift) {
-        _pixel_mode = PixelMode_t::blink;
-
-        _blink_info.last_blink = current_millis;
-        _blink_info.state      = true;
-        _led                   = _color;
-        PixelManager::update();
-        return;
-    }
-
-    if (_pixel_mode != PixelMode_t::blink) return;
-
-    unsigned long max_count = _blink_info.blink_count;
-    unsigned long counter   = _blink_info.blink_counter;
-
-    if (max_count && counter > max_count) {
-        _pixel_mode = PixelMode_t::off;
-        _led        = _background_color;
-        PixelManager::update();
-    }
-
-    long          wait_time    = _blink_info.state ? _blink_info.interval : _blink_info.off_interval;
-    unsigned long elapsed_time = _blink_info.last_blink ? current_millis - _blink_info.last_blink : wait_time;
-
-    if (elapsed_time >= wait_time) {
-        _blink_info.last_blink = current_millis;
-        _blink_info.state      = !_blink_info.state;
-        if (!_blink_info.state) _blink_info.blink_counter++;
-        _led = _blink_info.state ? _color : _background_color;
-        PixelManager::update();
-    }
-}
-
-Pixel& Pixel::shift(long ms) {
-    _shift      = millis() + ms;
-    _pixel_mode = PixelMode_t::shifted;
-    return *this;
-}
-
-Pixel& Pixel::off_interval(long ms) {
-    _blink_info.off_interval = ms;
-    return *this;
+    if (pixel_is_shifted()) return;
+    if (pixel_is_in_blink_mode()) handle_blink_mode();
+    if (pixel_is_in_permanent_mode()) handle_permanent_mode();
 }
--- src/Pixel.h
+++ src/Pixel.h
@@ -2,43 +2,62 @@
 
 #include <FastLED.h>
 
-enum PixelMode_t {
-    off,
-    steady,
-    blink,
-    shifted
-};
-
-struct BlinkInfo_t {
-    unsigned long interval;
-    unsigned long off_interval;
-    unsigned long blink_count;
-    unsigned long blink_counter;
-    unsigned long last_blink;
-    bool          state;
-};
-
 class Pixel {
     friend class PixelManager;
 
   public:
-    Pixel(CRGB&);
+    Pixel(CRGB& led);
     ~Pixel();
 
     Pixel& blink(const CRGB& color, unsigned long interval, unsigned int count = 0);
-    Pixel& color(const CRGB& color);
-    Pixel& backgroundColor(const CRGB& color);
-    Pixel& shift(long ms);
-    Pixel& off_interval(long ms);
+    Pixel& color(const CRGB color);
+    Pixel& shift(unsigned long ms);
+    Pixel& bgColor(const CRGB bgColor);
+    Pixel& off_interval(unsigned long interval);
 
   protected:
+    enum PixelMode_t {
+        PM_OFF,
+        PM_PERMANENT,
+        PM_BLINK
+    };
+
+    CRGB&       led;
+    CRGB        pixel_color;
+    CRGB        pixel_bg_color;
+    PixelMode_t pixel_mode;
+
+    unsigned long shift_time;
+    unsigned long blink_on_interval;
+    unsigned long blink_off_interval;
+    bool          pixel_state;
+    unsigned long blink_count;
+    unsigned long blink_counter;
+    unsigned long last_millis;
+
+  protected:
+    // clean code helper functions
+
+    inline bool          pixel_is_shifted();
+    inline bool          pixel_is_in_blink_mode();
+    inline bool          pixel_is_not_in_blink_mode();
+    inline bool          pixel_is_in_permanent_mode();
+    inline bool          pixel_is_not_in_permanent_mode();
+    inline unsigned long get_wait_time();
+    inline bool          blink_time_reached();
+    inline bool          blink_time_not_reached();
+    inline void          set_pixel_color_to_current_blink_state();
+    inline void          flip_state();
+    inline void          increment_blink_counter();
+    inline bool          blink_counter_is_at_maximum();
+    inline void          stop_blinking();
+    inline bool          state_is_on();
+    inline bool          state_is_off();
+    inline void          set_pixel_color(const CRGB& color);
+
+    // work functions
+
+    void handle_blink_mode();
+    void handle_permanent_mode();
     void _loop();
-
-  protected:
-    CRGB&         _led;
-    CRGB          _color;
-    CRGB          _background_color = CRGB::Black;
-    PixelMode_t   _pixel_mode       = PixelMode_t::off;
-    unsigned long _shift;
-    BlinkInfo_t   _blink_info;
 };
